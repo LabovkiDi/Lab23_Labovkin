ad83fea5d8aaab3757c99b1009f86d5d628ca648
Дмитрий Лабовкин <labovkindi@mail.ru>
2022-01-15 13:45:50 +0300

    Добавьте файлы проекта.

---
+++ Lab23_Labovkin/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab23_Labovkin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число");
+            int n = Convert.ToInt32(Console.ReadLine());
+            FactorialAsync(n);
+
+            Console.ReadKey();
+        }
+        //метод вычмсления факториала числа
+        static int Factorial(int n)
+        {
+            int s = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                s *= i;
+                //задержка
+                Thread.Sleep(150);
+            }
+            return (s);
+        }
+        // определение асинхронного метода
+        static async void FactorialAsync(int n)
+        {
+            //присваиваем результат асинхронной оаперации в переменную
+            int x = await Task.Run(() => Factorial(n));
+            Console.WriteLine($"Факториал равен {x}");
+        }
+    }
+}
